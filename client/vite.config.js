e05b27fe1ade72f39ea4bbff21536896e4ebfc07
Kerra Springgay <kerraspring@gmail.com>
2024-01-01 18:29:17 -0600

    package tweaks

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/express_backend": "http://localhost:5000/",
-    },
-    port: 3000,
-  },
+  
   plugins: [react()],
 });
