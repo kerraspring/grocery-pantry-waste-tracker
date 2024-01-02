29f1de807172e9a43f1b57c97e2101741e263772
Kerra Springgay <kerraspring@gmail.com>
2024-01-01 19:48:15 -0600

    .env path to server root

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: "../server/.env" });
 
 
 const connectDB = async () => {
